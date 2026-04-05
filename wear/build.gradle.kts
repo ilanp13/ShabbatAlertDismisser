830ba9b5a1a054efd45633afd343547834d22b2f
Ilan Peretz <ilan.peretz@optibus.com>
2026-04-05 22:50:48 +0300

    chore: bump wear to v66

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.ilanp13.shabbatalertdismisser"
         minSdk = 26
         targetSdk = 35
-        versionCode = 65
+        versionCode = 66
         versionName = "2.4.0"
     }
 
